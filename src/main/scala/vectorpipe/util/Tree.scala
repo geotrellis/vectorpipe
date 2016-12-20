1f99e845984312663de4d95533e57a4256718e9d
Colin Woodbury <colingw@gmail.com>
2016-12-19 16:10:50 -0800

    Add `Tree.pretty`
    - Adapted from GeoTrellis' `BTree`

--- src/main/scala/vectorpipe/util/Tree.scala
+++ src/main/scala/vectorpipe/util/Tree.scala
@@ -13,6 +13,28 @@ case class Tree[T](root: T, children: Seq[Tree[T]]) {
   def preorder: Seq[T] = root +: children.flatMap(_.preorder)
 
   def postorder: Seq[T] = children.flatMap(_.postorder) :+ root
+
+    /* Adapted from: http://stackoverflow.com/a/8948691/643684 */
+  def pretty: String = {
+    def work(tree: Tree[T], prefix: String, isTail: Boolean): String = {
+      val (line, bar) = if (isTail) ("└── ", " ") else ("├── ", "│")
+
+      val curr = s"${prefix}${line}${tree.root}"
+
+      val kids: String = if (tree.children.isEmpty) {
+        s"${prefix}${bar}   └── ∅"
+      } else {
+        val firsts = tree.children.init.map(t => work(t, s"${prefix}${bar}   ", false))
+        val last = work(tree.children.last, s"${prefix}${bar}   ", true)
+
+        (firsts :+ last).mkString("\n")
+      }
+
+      s"${curr}\n${kids}"
+    }
+
+    work(this, "", true)
+  }
 }
 
 object Tree {

537eb661e8f64b96a05a0ba99a09734c2775c4a6
Colin Woodbury <colingw@gmail.com>
2017-02-21 09:47:46 -0800

    Add `Pow2Layout` class and its `reduction` method

--- src/main/scala/vectorpipe/osm/internal/Gridding.scala
+++ src/main/scala/vectorpipe/osm/internal/Gridding.scala
@@ -10,6 +10,86 @@ import scalaz.syntax.applicative._
 
 // --- //
 
+/** A [[LayoutDefinition]] whose grid dimensions are known to be powers of 2.
+  *
+  * Do not instantiate this class yourself. Use [[Gridding.inflate]] instead.
+  */
+class Pow2Layout(val ld: LayoutDefinition) extends AnyVal {
+  /** If possible, reduce this `Pow2Layout` to children who are also Pow2Layout. */
+  def reduction: Seq[Pow2Layout] = (ld.layoutCols, ld.layoutRows) match {
+    case (1, 1) => Seq.empty[Pow2Layout] /* Cannot reduce further */
+    case (c, r) if c == r => squareReduction
+    case (c ,r) if c > r => horizontalReduction
+    case _ => verticalReduction
+  }
+
+  /** Split this Pow2Layout into four equal parts. */
+  private def squareReduction: Seq[Pow2Layout] = {
+    val tl = TileLayout(ld.layoutCols / 2, ld.layoutRows / 2, ld.tileCols, ld.tileRows)
+
+    /* The size of a new child Extent */
+    val xDelta: Double = (ld.extent.xmax - ld.extent.xmin) / 2
+    val yDelta: Double = (ld.extent.ymax - ld.extent.ymin) / 2
+
+    val topLeft = Extent(
+      ld.extent.xmin, ld.extent.ymin + yDelta,
+      ld.extent.xmax - xDelta, ld.extent.ymax
+    )
+
+    val topRight = Extent(
+      ld.extent.xmin + xDelta, ld.extent.ymin + yDelta,
+      ld.extent.xmax, ld.extent.ymax
+    )
+
+    val bottomLeft = Extent(
+      ld.extent.xmin, ld.extent.ymin,
+      ld.extent.xmax - xDelta, ld.extent.ymax - yDelta
+    )
+
+    val bottomRight = Extent(
+      ld.extent.xmin + xDelta, ld.extent.ymin,
+      ld.extent.xmax, ld.extent.ymax - yDelta
+    )
+
+    Seq(
+      new Pow2Layout(LayoutDefinition(topLeft, tl)),
+      new Pow2Layout(LayoutDefinition(topRight, tl)),
+      new Pow2Layout(LayoutDefinition(bottomLeft, tl)),
+      new Pow2Layout(LayoutDefinition(bottomRight, tl))
+    )
+  }
+
+  /** Cut this Layout into Left and Right child pieces.
+    * Child dimensions are still powers of two.
+    */
+  private def horizontalReduction: Seq[Pow2Layout] = {
+    val tl = TileLayout(ld.layoutCols / 2, ld.layoutRows, ld.tileCols, ld.tileRows)
+    val xDelta: Double = (ld.extent.xmax - ld.extent.xmin) / 2
+    val left = Extent(ld.extent.xmin, ld.extent.ymin, ld.extent.xmax - xDelta, ld.extent.ymax)
+    val right = Extent(ld.extent.xmin + xDelta, ld.extent.ymin, ld.extent.xmax, ld.extent.ymax)
+
+    Seq(
+      new Pow2Layout(LayoutDefinition(left, tl)),
+      new Pow2Layout(LayoutDefinition(right, tl))
+    )
+  }
+
+  /** Cut this Layout into Top and Bottom child pieces.
+    * Child dimensions are still powers of two.
+    */
+  private def verticalReduction: Seq[Pow2Layout] = {
+    val tl = TileLayout(ld.layoutCols, ld.layoutRows / 2, ld.tileCols, ld.tileRows)
+    val yDelta: Double = (ld.extent.ymax - ld.extent.ymin) / 2
+    val top = Extent(ld.extent.xmin, ld.extent.ymin + yDelta, ld.extent.xmax, ld.extent.ymax)
+    val bot = Extent(ld.extent.xmin, ld.extent.ymin, ld.extent.xmax, ld.extent.ymax - yDelta)
+
+    Seq(
+      new Pow2Layout(LayoutDefinition(top, tl)),
+      new Pow2Layout(LayoutDefinition(bot, tl))
+    )
+  }
+}
+
 /**
   * Internal mechanics for gridding a collection of [[OSMFeature]].
   */
@@ -28,7 +108,7 @@ object Gridding {
     * [[TileLayout]] dimensions are powers of 2. The [[Extent]] is expanded to
     * match this as well.
     */
-  def inflate(ld: LayoutDefinition): LayoutDefinition = {
+  def inflate(ld: LayoutDefinition): Pow2Layout = {
 
     /* --- Inflate the TileLayout --- */
 
@@ -56,7 +136,7 @@ object Gridding {
       ld.extent.xmax + (colDiff * xDelta), ld.extent.ymax
     )
 
-    LayoutDefinition(extent, tileLayout)
+    new Pow2Layout(LayoutDefinition(extent, tileLayout))
   }
 
   /** The next power of two after the given value. Yields the value itself

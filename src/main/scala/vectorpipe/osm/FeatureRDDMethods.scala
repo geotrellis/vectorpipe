6996521e27b09bb2636181829a5cf578dd2d9ebc
Colin Woodbury <colingw@gmail.com>
2017-02-28 10:09:49 -0800

    Bin the Geoms via GT `Rasterizer`

--- src/main/scala/vectorpipe/osm/FeatureRDDMethods.scala
+++ src/main/scala/vectorpipe/osm/FeatureRDDMethods.scala
@@ -1,5 +1,7 @@
 package vectorpipe.osm
 
+import geotrellis.raster._
+import geotrellis.raster.rasterize._
 import geotrellis.spark._
 import geotrellis.spark.tiling._
 import geotrellis.util._
@@ -8,6 +10,8 @@ import geotrellis.vector._
 import org.apache.spark._
 import org.apache.spark.rdd._
 import vectorpipe.osm.internal.Pow2Layout
+
+import scala.collection.mutable.{ Set => MSet }
 
 // --- //
 
@@ -18,17 +22,34 @@ class FeatureRDDMethods(val self: RDD[OSMFeature]) extends MethodExtensions[RDD[
     *
     * @param ld The LayoutDefinition defining the area to gridify.
     */
-  def toGrid(ld: LayoutDefinition)(implicit sc: SparkContext): RDD[(SpatialKey, Array[OSMFeature])] = {
+  def toGrid(ld: LayoutDefinition)(implicit sc: SparkContext): RDD[(SpatialKey, Iterable[OSMFeature])] = {
+
+    val mt: MapKeyTransform = ld.mapTransform
 
     /* Initial bounding box for capturing Features */
     val extent: Polygon = ld.extent.toPolygon()
 
-    /* Filter once to avoid later false positives on Extent overlapping after
-     * performing `Gridding.inflate`.
-     */
+    /* Filter once to reduce later workload */
     val bounded: RDD[OSMFeature] = self.filter(f => f.geom.intersects(extent))
 
-    work(Pow2Layout.inflate(ld), self)
+    bounded.map({ f =>
+      val env: Extent = f.geom.envelope
+      val bounds: GridBounds = mt(env)
+      val set: MSet[SpatialKey] = MSet.empty
+
+      /* Undefined behaviour if used concurrently */
+      val g: (Int, Int) => Unit = { (x, y) =>
+        set += SpatialKey(bounds.colMin + x, bounds.rowMin + y)
+      }
+
+      /* Extend envelope to snap to the tile grid */
+      val re = RasterExtent(ld.createAlignedGridExtent(env).extent, bounds.width, bounds.height)
+
+      Rasterizer.foreachCellByGeometry(f.geom, re)(g)
+
+      (f, set)
+    }).flatMap({ case (f, set) => set.map(k => (k, f)) })
+      .groupByKey()
   }
 
   /** Divide-and-conquer over progressively smaller extents, capturing

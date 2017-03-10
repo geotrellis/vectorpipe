c3ac6e543a601120e6a15ea15f689c96745844d2
Colin Woodbury <colingw@gmail.com>
2017-03-10 13:38:59 -0800

    Success at a fast, robust implementation for `toNearestPoint`

--- src/main/scala/vectorpipe/geom/Clip.scala
+++ src/main/scala/vectorpipe/geom/Clip.scala
@@ -191,6 +191,62 @@ object Clip {
     MultiLine(lines)
   }
 
+/* Clipping the "Java way" */
+  def toNearestPointJRobuster(extent: Extent, line: Line): MultiLine = {
+    val origPoints: Array[Point] = line.points
+    val points: Array[Point] = origPoints.tail
+    val centre: Point = Point(extent.xmin + (extent.width / 2), extent.ymin + (extent.height / 2))
+    val radius: Double = extent.northWest.distance(centre)
+
+    /* The mutability is real */
+    var acc: ListBuffer[Point] = new ListBuffer[Point]
+    var lines: ListBuffer[Line] = new ListBuffer[Line]
+    var i: Int = 0
+    var last: Point = origPoints.head
+
+    while (i < points.length) {
+      val p: Point = points(i)
+
+      if (extent.intersects(p) || extent.intersects(last)) {
+      /* First condition: The current Point is within the Extent.
+       * Regardless of where the previous Point was, we want to keep it:
+       *   In  -> In : We're inside the Extent still.
+       *   Out -> In : We were outside, now moving in.
+       *
+       * Second condition: We've moved outside the Extent.
+       */
+        acc.append(last)
+      } else if (acc.nonEmpty) {
+        /* We've moved further away from the first Point outside the Extent */
+        acc.append(last)
+        lines.append(Line(acc))
+        acc = new ListBuffer[Point]
+      } else if (intersects(centre, radius, last, p)) {
+        /* A line segment crosses the Extent, but has no Points within it */
+        acc.append(last)
+      }
+
+      /* Otherwise, we're moving along a segment of external Points. */
+      last = p
+      i += 1
+    }
+
+    if (acc.nonEmpty) {
+      acc.append(last)
+      lines.append(Line(acc))
+    }
+
+    MultiLine(lines)
+  }
+
+  /** A (hopefully) faster way to test Line-Extent intersection, when it's
+    * known that:
+    *   - The Line only has two points
+    *   - The two points lie outside the Extent
+    */
+  private def intersects(centre: Point, radius: Double, p1: Point, p2: Point): Boolean =
+    centre.distanceToSegment(p1, p2) <= radius
+
   /** Naively clips Features to fit the given Extent. */
   def byExtent(extent: Extent, f: OSMFeature): OSMFeature = Feature(
     f.geom.intersection(extent.toPolygon).toGeometry().get,
--- src/test/scala/vectorpipe/geom/ClipBench.scala
+++ src/test/scala/vectorpipe/geom/ClipBench.scala
@@ -6,7 +6,48 @@ import scaliper._
 // --- //
 
 class ClipBench extends Benchmarks with ConsoleReport {
+  /*
+  benchmark("Basic Operations") {
+    run("Line.points - 2 points") {
+      new Benchmark {
+        var line: Line = _
+
+        override def setUp() = {
+          line = Line((0,0), (1,1))
+        }
+
+        def run() = line.points
+      }
+    }
+
+    run("Line.points - 20 points") {
+      new Benchmark {
+        var line: Line = _
+
+        override def setUp() = {
+          line = Line((1 to 20).map(n => Point(n,n)))
+        }
+
+        def run() = line.points
+      }
+    }
+
+    run("Line.points - 200 points") {
+      new Benchmark {
+        var line: Line = _
+
+        override def setUp() = {
+          line = Line((1 to 200).map(n => Point(n,n)))
+        }
+
+        def run() = line.points
+      }
+    }
+  }
+   */
+
   benchmark("toNearestPoint - ALL IN - two-point line") {
+    /*
     run("Tail Recursion") {
       new Benchmark {
         var extent: Extent = _
@@ -34,6 +75,7 @@ class ClipBench extends Benchmarks with ConsoleReport {
         def run() = Clip.toNearestPointF(extent, line)
       }
     }
+    */
 
     run("Java Style") {
       new Benchmark {
@@ -60,11 +102,26 @@ class ClipBench extends Benchmarks with ConsoleReport {
         }
 
         def run() = Clip.toNearestPointJRobust(extent, line)
+      }
+    }
+
+    run("Java Style - Robuster") {
+      new Benchmark {
+        var extent: Extent = _
+        var line: Line = _
+
+        override def setUp() = {
+          extent = Extent(0, 0, 5, 5)
+          line = Line((1,1), (2,2))
+        }
+
+        def run() = Clip.toNearestPointJRobuster(extent, line)
       }
     }
   }
 
   benchmark("toNearestPoint - ALL IN - short line") {
+    /*
     run("Tail Recursion") {
       new Benchmark {
         var extent: Extent = _
@@ -92,6 +149,7 @@ class ClipBench extends Benchmarks with ConsoleReport {
         def run() = Clip.toNearestPointF(extent, line)
       }
     }
+    */
 
     run("Java Style") {
       new Benchmark {
@@ -118,11 +176,26 @@ class ClipBench extends Benchmarks with ConsoleReport {
         }
 
         def run() = Clip.toNearestPointJRobust(extent, line)
+      }
+    }
+
+    run("Java Style - Robuster") {
+      new Benchmark {
+        var extent: Extent = _
+        var line: Line = _
+
+        override def setUp() = {
+          extent = Extent(-1000, -1000, 1000, 1000)
+          line = Line((-10 to 10).map(n => Point(n,n)))
+        }
+
+        def run() = Clip.toNearestPointJRobuster(extent, line)
       }
     }
   }
 
   benchmark("toNearestPoint - ALL IN - medium line") {
+    /*
     run("Tail Recursion") {
       new Benchmark {
         var extent: Extent = _
@@ -150,6 +223,7 @@ class ClipBench extends Benchmarks with ConsoleReport {
         def run() = Clip.toNearestPointF(extent, line)
       }
     }
+    */
 
     run("Java Style") {
       new Benchmark {
@@ -176,11 +250,26 @@ class ClipBench extends Benchmarks with ConsoleReport {
         }
 
         def run() = Clip.toNearestPointJRobust(extent, line)
+      }
+    }
+
+    run("Java Style - Robuster") {
+      new Benchmark {
+        var extent: Extent = _
+        var line: Line = _
+
+        override def setUp() = {
+          extent = Extent(-1000, -1000, 1000, 1000)
+          line = Line((-100 to 100).map(n => Point(n,n)))
+        }
+
+        def run() = Clip.toNearestPointJRobuster(extent, line)
       }
     }
   }
 
   benchmark("toNearestPoint - MOST OUT - short line") {
+    /*
     run("Tail Recursion") {
       new Benchmark {
         var extent: Extent = _
@@ -208,6 +297,7 @@ class ClipBench extends Benchmarks with ConsoleReport {
         def run() = Clip.toNearestPointF(extent, line)
       }
     }
+    */
 
     run("Java Style") {
       new Benchmark {
@@ -234,11 +324,26 @@ class ClipBench extends Benchmarks with ConsoleReport {
         }
 
         def run() = Clip.toNearestPointJRobust(extent, line)
+      }
+    }
+
+    run("Java Style - Robuster") {
+      new Benchmark {
+        var extent: Extent = _
+        var line: Line = _
+
+        override def setUp() = {
+          extent = Extent(0, 0, 5, 5)
+          line = Line((-10 to 10).map(n => Point(n,n)))
+        }
+
+        def run() = Clip.toNearestPointJRobuster(extent, line)
       }
     }
   }
 
   benchmark("toNearestPoint - MOST OUT - medium line") {
+    /*
     run("Tail Recursion") {
       new Benchmark {
         var extent: Extent = _
@@ -266,6 +371,7 @@ class ClipBench extends Benchmarks with ConsoleReport {
         def run() = Clip.toNearestPointF(extent, line)
       }
     }
+     */
 
     run("Java Style") {
       new Benchmark {
@@ -292,6 +398,20 @@ class ClipBench extends Benchmarks with ConsoleReport {
         }
 
         def run() = Clip.toNearestPointJRobust(extent, line)
+      }
+    }
+
+    run("Java Style - Robuster") {
+      new Benchmark {
+        var extent: Extent = _
+        var line: Line = _
+
+        override def setUp() = {
+          extent = Extent(0, 0, 5, 5)
+          line = Line((-100 to 100).map(n => Point(n,n)))
+        }
+
+        def run() = Clip.toNearestPointJRobuster(extent, line)
       }
     }
   }

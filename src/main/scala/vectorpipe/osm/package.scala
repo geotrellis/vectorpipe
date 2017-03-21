ffa087007c96f22c3b37e8fe18190691eff41bd5
Colin Woodbury <colingw@gmail.com>
2017-03-21 15:28:25 -0700

    Provide custom `reproject` function for `OSMFeature`s
    - Their bounding envelopes, which they store ahead of time along with
      the `Tree` of metadata, needs to be reprojected as well. Asking the
      user to do this is a bit much, so we take care of it with this
      function.
    
    - I'm not entirely thrilled by its implementation location (the
      package object of `vectorpipe.osm`), or the fact that it's a
      function.

--- src/main/scala/vectorpipe/osm/package.scala
+++ src/main/scala/vectorpipe/osm/package.scala
@@ -1,5 +1,6 @@
 package vectorpipe
 
+import geotrellis.proj4.CRS
 import geotrellis.vector._
 import vectorpipe.util.Tree
 
@@ -13,5 +14,12 @@ package object osm {
   type OSMLine = Feature[Line, Tree[ElementData]]
   type OSMPolygon = Feature[Polygon, Tree[ElementData]]
   type OSMMultiPoly = Feature[MultiPolygon, Tree[ElementData]]
+
+  /** Reproject an [[OSMFeature]]. */
+  def reproject(f: OSMFeature, src: CRS, dest: CRS): OSMFeature = {
+    val (tree, extent) = f.data
+
+    f.reproject(src, dest).copy(data = (tree, extent.reproject(src, dest)))
+  }
 
 }

584ea67d7ddaf2c1918113021397933c02c47bcb
Colin Woodbury <colingw@gmail.com>
2017-11-01 10:59:22 -0700

    Streams are terrible, use Lists

--- src/main/scala/vectorpipe/osm/internal/PlanetHistory.scala
+++ src/main/scala/vectorpipe/osm/internal/PlanetHistory.scala
@@ -19,7 +19,7 @@ private[vectorpipe] object PlanetHistory {
   /** Given one RDD of nodes and one of ways, produce an RDD of all nodes/ways keyed by the WayID
     * to which they are all related.
     */
-  private def joinedWays(
+  private[this] def joinedWays(
     nodes: RDD[(Long, Node)],
     ways: RDD[(Long, Way)]
   ): RDD[(Long, (Iterable[Way], Iterable[Node]))] = {
@@ -45,13 +45,13 @@ private[vectorpipe] object PlanetHistory {
     *   - there is a Line present for every set of updated Nodes (i.e. those with matching timestamps)
     *   - there is a Polygon present instead of a Line from the above two conditions when the Way is "closed"
     */
-  private[vectorpipe] def linesAndPolys(ways: List[Way], nodes: List[Node]): (Stream[OSMLine], Stream[OSMPolygon]) = {
+  private[vectorpipe] def linesAndPolys(ways: List[Way], nodes: List[Node]): (List[OSMLine], List[OSMPolygon]) = {
 
     @tailrec def work(
       ws: List[Way],
-      ls: Stream[OSMLine],
-      ps: Stream[OSMPolygon]
-    ): (Stream[OSMLine], Stream[OSMPolygon]) = ws match {
+      ls: List[OSMLine],
+      ps: List[OSMPolygon]
+    ): (List[OSMLine], List[OSMPolygon]) = ws match {
       case Nil => (ls, ps)
       case w :: rest => {
 
@@ -59,10 +59,10 @@ private[vectorpipe] object PlanetHistory {
         val nextTime: Instant = if (rest.isEmpty) Instant.MAX else rest.head.data.meta.timestamp
 
         /* Each full set of Nodes that would have existed for each time slice. */
-        val allSlices: Stream[(Instant, Map[Long, Node])] =
+        val allSlices: List[(Instant, Map[Long, Node])] =
           changedNodes(w.data.meta.timestamp, nextTime, nodes)
             .groupBy(_.data.meta.timestamp)
-            .toStream
+            .toList
             .sortBy { case (i, _) => i }
             .scanLeft((w.data.meta.timestamp, recentNodes(w, nodes))) { case ((_, p), (i, changes)) =>
 
@@ -77,36 +77,36 @@ private[vectorpipe] object PlanetHistory {
             }
 
         if (w.isClosed) {
-          val everyPoly: Stream[OSMPolygon] = allSlices.map { case (i, ns) =>
+          val everyPoly: List[OSMPolygon] = allSlices.map { case (i, ns) =>
             Feature(
               Polygon(Line(ns.values.map(n => Point(n.lon, n.lat)))),
               w.data.copy(meta = w.data.meta.copy(timestamp = i)) // TODO Overwrite more values?
             )
           }
 
-          work(rest, ls, ps #::: everyPoly)
+          work(rest, ls, ps ++ everyPoly)
         } else {
-          val everyLine: Stream[OSMLine] = allSlices.map { case (i, ns) =>
+          val everyLine: List[OSMLine] = allSlices.map { case (i, ns) =>
             Feature(
               Line(ns.values.map(n => Point(n.lon, n.lat))),
               w.data.copy(meta = w.data.meta.copy(timestamp = i)) // TODO Overwrite more values?
             )
           }
 
-          work(rest, ls #::: everyLine, ps)
+          work(rest, ls ++ everyLine, ps)
         }
 
       }
     }
 
     /* Ensure the Ways are sorted before proceeding */
-    work(ways.sortBy(_.data.meta.timestamp), Stream.empty, Stream.empty)
+    work(ways.sortBy(_.data.meta.timestamp), Nil, Nil)
   }
 
   /** Given a collection of all Nodes ever associated with a [[Way]], which subset
     * of them are the "most recent" from the perspective of the given Way?
     */
-  private def recentNodes(way: Way, nodes: List[Node]): Map[Long, Node] = {
+  private[this] def recentNodes(way: Way, nodes: List[Node]): Map[Long, Node] = {
     nodes
       .filter(!_.data.meta.timestamp.isAfter(way.data.meta.timestamp))
       .groupBy(_.data.meta.id)
@@ -114,7 +114,7 @@ private[vectorpipe] object PlanetHistory {
   }
 
   /** Find all the Nodes that were created/changed between two timestamps. */
-  private def changedNodes(t0: Instant, t1: Instant, nodes: List[Node]): List[Node] =
+  private[this] def changedNodes(t0: Instant, t1: Instant, nodes: List[Node]): List[Node] =
     nodes.filter(n => n.data.meta.timestamp.isAfter(t0) && n.data.meta.timestamp.isBefore(t1))
 
 }

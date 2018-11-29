47e3065c5de0296965e9ab78875ca4c8af64166f
Jacob Bouffard <jbouffard@azavea.com>
2018-11-29 10:21:36 -0500

    Bumped the Spark version

--- project/Version.scala
+++ project/Version.scala
@@ -4,7 +4,7 @@ object Version {
   val vectorpipe = "0.3.0"
   val geotrellis = "3.0.0-SNAPSHOT"
   val geomesa    = "2.0.1"
-  val spark      = "2.2.0"
+  val spark      = "2.4.0"
   val decline    = "0.4.0-RC1"
   val monocle   = "1.5.0-cats-M2"
   val cats       = "1.0.0-RC1"

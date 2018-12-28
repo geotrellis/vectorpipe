d757e68f99a1e63b13b60bb97a4dbacb96d889b0
Grigory Pomadchin <gr.pomadchin@gmail.com>
2018-12-28 15:27:51 +0300

    Define a Component Lens instead of only Get

--- src/main/scala/vectorpipe/LayerMetadata.scala
+++ src/main/scala/vectorpipe/LayerMetadata.scala
@@ -3,7 +3,8 @@ package vectorpipe
 import geotrellis.spark._
 import geotrellis.spark.io._
 import geotrellis.spark.tiling.LayoutDefinition
-import geotrellis.util.GetComponent
+import geotrellis.util.Component
+
 import spray.json._
 import spray.json.DefaultJsonProtocol._
 
@@ -14,9 +15,15 @@ case class LayerMetadata[K: JsonFormat](layout: LayoutDefinition, bounds: KeyBou
 
 object LayerMetadata {
 
-  /** A Lens into the key bounds. */
-  implicit def metaGet[K]: GetComponent[LayerMetadata[K], Bounds[K]] =
-    GetComponent(_.bounds)
+  /** A Lens into and from the key bounds. */
+  implicit def metaComponent[K: JsonFormat]: Component[LayerMetadata[K], Bounds[K]] =
+    Component[LayerMetadata[K], Bounds[K]](
+      _.bounds,
+      (md, bounds) => bounds match {
+        case kb: KeyBounds[K] => md.copy(bounds = kb)
+        case _ => md
+      }
+    )
 
   /** Json Conversion. */
   implicit def metaFormat[K: JsonFormat]: RootJsonFormat[LayerMetadata[K]] =
